0562a19e43cf9553e1a457d41090f14ffeb38060
ashishkumar3027 <67895557+ashishkumar3027@users.noreply.github.com>
2021-10-19 22:39:22 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,31 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##this is a assignment of 3rd week, in R programming from coursera
 
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##there are two functions makeCacheMatrix,cacheSolve,
+##we will first look for the makeCacheMaatrix function
+
+
+
+makeCacheMatrix <- function(x = matrix()) {       ## define the argument with default mode of "matrix"
  
-    j <- NULL 
-    set <- function(y){
-      x <<- y 
-      j <<- NULL 
+    j <- NULL                                    ## initialize j as NULL; will hold value of matrix inverse 
+    set <- function(y){                          ## define the set function to assign new 
+      x <<- y                                    ## value of matrix in parent environment
+      j <<- NULL                                 ## if there is a new matrix, reset inv to NUL    
     }
-    get <- function()x 
-    setInverse <- function(inverse) j <<- inverse 
-    getInverse <- function() j
-    list(set = set, get = get,  
-         setInverse = setInverse,  
-         getInverse = getInverse) 
+ 
+ 
+    get <- function()x                                   ## define the get fucntion - returns value of the matrix argument
+ 
+    setInverse <- function(inverse) j <<- inverse        ## assigns value of inv in parent environment
+    getInverse <- function() j                            ## gets the value of inv where called
+ 
+    list(set = set, get = get,  setInverse = setInverse,  getInverse = getInverse)    ## you need this in order to refer
+                                                                                      ## to the functions with the $ operator
   } 
   
   
@@ -29,6 +34,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+##This function computes the inverse of the special 
+##“matrix” returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed),
+##then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x' 
